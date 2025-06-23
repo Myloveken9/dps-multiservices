51cc7395f4dc755c477b10cff20c1c3fc9985f13
Kenlove Chereste <cherestek@gmail.com>
2025-06-23 03:08:24 -0400

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'dpsmultiservices.com.br', // ← correct pour domaine personnalisé comme dpsmultiservices.com.br
+  base: '/', // ✅ correct pour domaine personnalisé ou root
 })
