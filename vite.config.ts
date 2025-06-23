ea56ef215c7a84f92672dbcd179d3e78a311003b
Myloveken9 <cherestek@gmail.com>
2025-06-23 09:26:17 -0400

    Correction

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '//www.dpsmultiservices.com.br', // ← correct pour domaine personnalisé comme dpsmultiservices.com.br
+  base: '', // ← correct pour domaine personnalisé comme dpsmultiservices.com.br
 
 // ✅ correct pour domaine personnalisé ou root et pour ne pas avoir de conflit
 })
